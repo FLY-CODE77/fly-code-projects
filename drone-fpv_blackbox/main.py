4cf927f6e118201b72519097f7dee10ce54715b4
FLY-CODE77 <lightyear94122@gmail.com>
2021-06-25 10:33:42 +0900

    output avi file save name change

--- drone-fpv_blackbox/main.py
+++ drone-fpv_blackbox/main.py
@@ -1,7 +1,8 @@
 import cv2
 import numpy as np
+import time 
 
-cap = cv2.VideoCapture(1) # for mac 1
+cap = cv2.VideoCapture(0) # for mac 1
 						  # for raspberry 0
 
 width = int(cap.get(3))
@@ -12,7 +13,10 @@ fcc = cv2.VideoWriter_fourcc("M", "J", "P", "G") # for mac fourcc
 												 # for raspberry pi 
 												 # cv2.VideoWriter_fourcc(*'XVID')
 
-out = cv2.VideoWriter('output.avi', fcc, fps, (width, height))
+# writer module file name will be start recording time
+name = time.strftime('%c', time.localtime(time.time()))
+name = name + ".avi"
+out = cv2.VideoWriter(name, fcc, fps, (width, height))
 
 while True:
 	ret, frame = cap.read()

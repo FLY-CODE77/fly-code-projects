a91ff1366306e271983b28ab9d72ce61bb3b01c9
FLY-CODE77 <lightyear94122@gmail.com>
2021-06-25 10:24:11 +0900

    basic function working code

--- drone-fpv_blackbox/main.py
+++ drone-fpv_blackbox/main.py
@@ -8,4 +8,17 @@ width = int(cap.get(3))
 height = int(cap.get(4))
 fps = 30 
 
+fcc = cv2.VideoWriter_fourcc("M", "J", "P", "G") # for mac fourcc 
+												 # for raspberry pi 
+												 # cv2.VideoWriter_fourcc(*'XVID')
 
+out = cv2.VideoWriter('output.avi', fcc, fps, (width, height))
+
+while True:
+	ret, frame = cap.read()
+	cv2.imshow("img", frame)
+	out.write(frame) 
+	if cv2.waitKey(1) & 0xFF == ord("q"):
+		break
+
+cap.release()

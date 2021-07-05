4a6e1bf04cea05116f42ba88d219a1c8056100ea
FLY-CODE77 <lightyear94122@gmail.com>
2021-07-05 15:05:44 +0900

    add : def getdata

--- marketing/car-ad/dataload.py
+++ marketing/car-ad/dataload.py
@@ -15,3 +15,13 @@ def login():
     cursor = mydb.cursor(pymysql.cursors.DictCursor)
     return mydb, cursor
 
+'''
+data load from database
+if you give qry return dataset
+'''
+def getdata(qry1):
+    mydb, cursor = login()
+    cursor.execute(qry1)
+    rlt1 = cursor.fetchall()
+    df_1 = pd.DataFrame(rlt1)
+    return df_1

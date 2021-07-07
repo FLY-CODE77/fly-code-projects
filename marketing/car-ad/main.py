fe661f4a41678df704f25b61bcc6ddeb3e5ee6ac
FLY-CODE77 <lightyear94122@gmail.com>
2021-07-07 14:51:18 +0900

    fin of car-ad project

--- marketing/car-ad/main.py
+++ marketing/car-ad/main.py
@@ -80,22 +80,25 @@ plt.show()
 toyota_day = df_calc.monthly_sub(toyota)
 honda_day = df_calc.monthly_sub(honda)
 
+#bad day of toyota and honda 
+toyota_day_bad = toyota_day[5:]
+honda_day_bad = honda_day[5:]
 # axvline the 10 up and down days in graph
 plt.figure(figsize=(18,10))
-plt.title('anti-japan')
+plt.title('anti-japan-after')
 plt.plot(toyota.index, toyota["value"], label="toyota", marker='h')
 plt.plot(honda.index, honda["value"], label="honda", marker='h')
 
-for i in range(len(toyota_day.index)):
-    plt.axvline(x=toyota_day.index[i], color='g', linestyle='-', linewidth=3)
+for i in range(len(toyota_day_bad.index)):
+    plt.axvline(x=toyota_day_bad.index[i], color='g', linestyle='-', linewidth=3)
 
-for i in range(len(honda_day.index)):
-    plt.axvline(x=honda_day.index[i], color='k', linestyle='--', linewidth=3)
+for i in range(len(honda_day_bad.index)):
+    plt.axvline(x=honda_day_bad.index[i], color='k', linestyle='--', linewidth=3)
     
-plt.axvline(x=datetime(2016, 11, 1), color='b', linestyle='--', linewidth=2)
-plt.axvline(x=datetime(2016, 11, 1), color='b', linestyle='--', linewidth=2)
-plt.axvline(x=datetime(2018, 12, 1), color='b', linestyle='--', linewidth=2)
-plt.axvline(x=datetime(2019, 9, 1), color='b', linestyle='--', linewidth=2)
+plt.axvline(x=datetime(2016, 11, 1), color='b', linestyle='--', linewidth=4)
+plt.axvline(x=datetime(2016, 11, 1), color='b', linestyle='--', linewidth=4)
+plt.axvline(x=datetime(2018, 12, 1), color='b', linestyle='--', linewidth=4)
+plt.axvline(x=datetime(2019, 9, 1), color='b', linestyle='--', linewidth=4)
 plt.axvline(x=datetime(2019, 12, 1), color='r', linestyle='--', linewidth=2)
 plt.legend()
 plt.savefig("./graph/anti-japan_after.png")
@@ -105,4 +108,16 @@ plt.show()
 유의미하다고 느껴지는것은 반일 정서가 생겼을때.. 즉각적으로 자동차 등록댓수가 줄어 들었다.
 '''
 
-# 
+# 이번 일본차 eda 결과
+'''
+기존 일본차에 대한 인식은 충성도 있는 고객이 많아서 
+대중들의 감정과 크게 상관 없이 판매되는 양은 일정하다고 여겨졌는데
+
+실제로 데이터를 분석을 해본결과
+한국의 일본 차 등록 대수는 파장이 도달하는 시간이 조금씩 차이가 날뿐
+반일 감정과 상당히 연관성이 있다고 판단이 됩니다.
+또한 이러한 감정으로 인한 판매량 감소는 회복까지 오랜 시간이 걸립니다.
+
+여러 매체나 sns를 미리 실시간으로 정보를 수집하면서, 만약 이러한 정서를 판단을 할 수 있다면,
+광고 전략 및 판매 전략에 상당히 도움이 될 것으로 사료됩니다.
+'''

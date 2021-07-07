c0c658cf35d72bed69b98638cacf3cbc7d798c90
FLY-CODE77 <lightyear94122@gmail.com>
2021-07-07 13:13:16 +0900

    monthly_sub function add

---
+++ marketing/car-ad/module/df_calc.py
@@ -0,0 +1,16 @@
+def monthly_sub(df):
+    '''
+    sub this month - last month car sales value
+    return top 5 increasement and decreasement sales months 
+    and values 
+    '''
+    df = df.sort_values(by='date')
+    new_df = df[1:]
+    new_df['before'] = df['value'][:-1].values
+    new_df['sub'] = (new_df['value'] - new_df['before'])
+    
+    good_day = new_df['sub'].sort_values(ascending=False)[:5]
+    bad_day = new_df['sub'].sort_values()[:5]
+    day_df = pd.concat([good_day, bad_day], 0)
+    return day_df
+    print('function activated')
